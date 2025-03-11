b3161d36e1b87fb41bf303048014edad50b56189
javanism <javanism4@gmail.com>
2025-03-11 12:43:00 +0900

    feat:secu_login

--- js/index.js
+++ js/index.js
@@ -1,18 +1,85 @@
-window.onload=async ()=>{
-    let response = await axios.get("http://localhost:8080/getAllProducts");
-    console.log(response);
-    let productList=response.data;
-    let productListDiv=``;
-    productList.forEach(item => {
-        productListDiv += `<div class="card m-3" style="width: 10rem;">
+window.onload = async () => {
+ 
+
+  let response = await axios.get("http://localhost:8080/getAllProducts");
+  console.log(response);
+  let productList = response.data;
+  let productListDiv = ``;
+
+  productList.forEach((item) => {
+    productListDiv += `<div class="card m-3" style="width: 10rem;">
         <img src="img/${item.pimg}" class="card-img-top" alt="...">
+       
         <div class="card-body">
           <b class="card-title">${item.prodname}</b>
-          <p class="card-text text-danger">${item.price}원</p>
+          <a href="#" class="review-link" data-product-id="${item.prodcode}" 
+          data-bs-toggle="modal" data-bs-target="#commentModal"> 
+            <img src="img/review.png" alt="review"> 
+          </a>
+          <p class="card-text text-danger">${item.price}원</p>   
           <a href="#" class="btn btn-outline-info">장바구니 담기</a>
         </div>
       </div>`;
-    });
+  });
 
-    document.getElementById("productListDiv").innerHTML=productListDiv;
-}
+  document.getElementById("productListDiv").innerHTML = productListDiv;
+
+  // 모달 열기 이벤트 리스너
+  let prodcode;
+  const reviewLinks = document.querySelectorAll(".review-link");
+  reviewLinks.forEach((link) => {
+    link.addEventListener("click", async (event) => {
+      prodcode = link.getAttribute("data-product-id"); // 클릭한 제품의 ID 가져오기
+      try {
+        const reviewsResponse = await axios.post(
+          "http://localhost:8080/getRecentReviews",
+          { prodcode }
+        );
+        const reviews = reviewsResponse.data;
+        console.log(reviews);
+
+        // 모달 본문에 리뷰 표시
+        let reviewListDiv = `<ul>`;
+        reviewListDiv += reviews
+          .map((item) => `<li>${item.review}</li>`)
+          .join("");
+        reviewListDiv += `</ul>`;
+        document.getElementById("commentModalBody").innerHTML = reviewListDiv;
+      } catch (error) {
+        console.error("리뷰를 가져오는 데 오류가 발생했습니다:", error);
+        document.getElementById("commentModalBody").innerHTML =
+          "리뷰를 불러오는 데 오류가 발생했습니다.";
+      }
+    });
+  });
+
+  // 댓글 남기기 버튼 클릭 시 처리
+  document
+    .getElementById("submitComment")
+    .addEventListener("click", async () => {
+      const review = document.getElementById("commentTextarea").value;
+      const nickname = sessionStorage.getItem("nickname");
+      console.log(nickname, prodcode, review);
+      if (review && prodcode) {
+        try {
+          await axios.post("http://localhost:8080/insertReview", {
+            nickname,
+            prodcode,
+            review,
+          });
+          document.getElementById("commentTextarea").value = ""; // 댓글 입력창 초기화
+          alert("후기가 등록되었습니다!");
+          // 모달 닫기
+          const modal = bootstrap.Modal.getInstance(
+            document.getElementById("commentModal")
+          );
+          modal.hide();
+        } catch (error) {
+          console.error("후기를 등록하는 데 오류가 발생했습니다:", error);
+          alert("후기를 등록하는 데 오류가 발생했습니다.");
+        }
+      } else {
+        alert("후기를 입력해주세요.");
+      }
+    });
+};
--- js/member.js
+++ js/member.js
@@ -1,20 +1,59 @@
 document.getElementById("signupBtn").addEventListener("click", async ()=>{
-    const email=document.getElementById("email").value;
-    const pwd=document.getElementById("pwd").value;
-    const nickname=document.getElementById("nickname").value;
+  const email=document.getElementById("email").value;
+  const pwd=document.getElementById("pwd").value;
+  const nickname=document.getElementById("nickname").value;
 
-    const data={email,pwd,nickname};
+  const data={email,pwd,nickname};
 
-    const response=await axios.post("http://localhost:8080/insertMember" , data);
+  const response=await axios.post("http://localhost:8080/insertMember" , data);
 
-    document.getElementById("effetMsg").innerHTML = response.data;
+  document.getElementById("effetMsg").innerHTML = response.data;
 });
 
+// document.getElementById("loginBtn").addEventListener("click", async () => {  
+//     const email = document.getElementById("loginEmail").value;
+//     const pwd = document.getElementById("loginPwd").value;
+//     const data = {  email, pwd };
+//     const response=await axios.post("http://localhost:8080/login" , data);
+//     console.log(response.data);
+//     alert(response.data.nickname+"님 반갑습니다");
+//   });
+
 document.getElementById("loginBtn").addEventListener("click", async () => {  
-    const email = document.getElementById("loginEmail").value;
-    const pwd = document.getElementById("loginPwd").value;
-    const data = {  email, pwd };
-    const response=await axios.post("http://localhost:8080/login" , data);
-    console.log(response.data);
-    alert(response.data.nickname+"님 반갑습니다");
-  });
+  const email = document.getElementById("loginEmail").value;
+  const pwd = document.getElementById("loginPwd").value;
+  const data = {  email, pwd };
+  const response=await axios.post("http://localhost:8080/tokenLogin" , data);
+  
+  document.getElementById("loginSpan").innerHTML=`${response.data.nickname}  
+  <button class="btn btn-danger btn-sm" id="logoutBtn">Logout</button>`;
+  const token = response.data.Authorization;
+  console.log('Authorization:', token);
+  sessionStorage.setItem('Authorization', token);
+  sessionStorage.setItem('nickname', response.data.nickname);
+  axios.defaults.headers.common['Authorization'] = token; // Authorization 헤더 설정
+  // 모달 닫기
+  const modal = bootstrap.Modal.getInstance(document.getElementById("loginModal"));
+  loginModal.setAttribute("aria-hidden", "true");
+  modal.hide();
+});
+
+
+
+const Authorization = sessionStorage.getItem("Authorization");
+const nickname = sessionStorage.getItem("nickname");
+if (Authorization && nickname) {
+  axios.defaults.headers.common['Authorization'] = Authorization; // Authorization 헤더 설정
+  document.getElementById("loginSpan").innerHTML = `${nickname}  
+  <button class="btn btn-danger btn-sm" id="logoutBtn">Logout</button>`;
+}
+
+document.getElementById("loginSpan").addEventListener("click", async (event)=>{
+  if(event.target.id=='logoutBtn'){       
+      await axios.post("http://localhost:8080/logout");
+      sessionStorage.removeItem("nickname");
+      sessionStorage.removeItem("Authorization");
+      axios.defaults.headers.common['Authorization'] = ''; // Authorization 헤더에서 삭제       
+      window.location.reload();
+  }
+});
